694aa502440f0c28111498184a86095ae76f91c3
Samuel Aiedo <samueldefreitasaiedo@gmail.com>
2024-06-18 17:14:55 -0300

    aumentando os sorteados (teste)

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 function sortear(){
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate= document.getElementById('ate').value;
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate= parseInt(document.getElementById('ate').value);
 
-    alert(`quantidade: ${quantidade}`);
-    alert(`do número: ${de}`);
-    alert(`ate o número: ${ate}`);
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = sortnum (de, ate);
+        sorteados.push(numero);
+    }
+   
+    alert(sorteados);
+}
+
+function sortnum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
