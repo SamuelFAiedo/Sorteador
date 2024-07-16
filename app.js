3f0cfcc2dfdeff5a63abb6008181941c752d498a
Samuel Aiedo <samueldefreitasaiedo@gmail.com>
2024-07-15 23:32:55 -0300

    Reiniciar Update

--- app.js
+++ app.js
@@ -8,12 +8,40 @@ function sortear(){
 
     for (let i = 0; i < quantidade; i++) {
         numero = sortnum (de, ate);
+
+        while (sorteados.includes(numero)){
+        numero = sortnum(de, ate);
+        }
+
+        alterarEstadoBotao
+
         sorteados.push(numero);
     }
    
-    alert(sorteados);
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+    alterarEstadoBotao();
 }
 
 function sortnum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarEstadoBotao() {
+    let botao = document.getElementById("btn-reiniciar");
+    if (botao.classList.contains("container__botao-desabilitado")) {
+        botao.classList.remove("container__botao-desabilitado");
+        botao.classList.add("container__botao");
+    } else {
+        botao.classList.remove("container__botao");
+        botao.classList.add("container__botao-desabilitado");
+    }
+}
+
+function reiniciar() {
+    document.getElementById("quantidade").value = " ";
+    document.getElementById('de').value = " ";
+    document.getElementById("ate").value = " ";
+    document.getElementById("resultado").innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    alterarEstadoBotao();
 }
